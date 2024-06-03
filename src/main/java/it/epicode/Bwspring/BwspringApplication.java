a93a3cd9d0b404472927e94b92516b7a20834e15
MarioVarano <alexxan@live.it>
2024-06-03 09:54:17 +0200

    ultima bw

---
+++ src/main/java/it/epicode/Bwspring/BwspringApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.Bwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/Bwspring/BwspringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Bwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
